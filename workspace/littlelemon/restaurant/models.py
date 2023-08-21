c2affdc83a43e90dd20c0b263e141339c054dae6
WiktoriaKoziel <wiktoria.koziel@gmail.com>
2023-08-21 13:21:28 +0200

    final

--- workspace/littlelemon/restaurant/models.py
+++ workspace/littlelemon/restaurant/models.py
@@ -7,6 +7,8 @@ class Menu(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField(default=0)
+    def __str__(self):
+        return f'{self.title} : {str(self.price)}'
 
 class Booking(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -14,3 +16,10 @@ class Booking(models.Model):
     no_of_guest = models.IntegerField(default=0)
     bookingDate = models.DateField(default='2000-01-01')
     
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.SmallIntegerField()
+
+    def get_item(self):
+        return f'{self.title} : {str(self.price)}'
--- workspace/littlelemon/restaurant/views.py
+++ workspace/littlelemon/restaurant/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
+from requests import Response
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .serializers import MenuSerializer
-from rest_framework.decorators import permission_classes
+from rest_framework.decorators import permission_classes, api_view
 from rest_framework.permissions import IsAuthenticated
 from django.core import serializers
 from .models import Booking, Menu
 import json
 from rest_framework.viewsets import ModelViewSet
 from .serializers import BookingSerializer
+
+@api_view()
+@permission_classes([IsAuthenticated])
+# @authentication_classes([TokenAuthentication])
+def msg(request):
+    return Response({"message":"This view is protected"})
 
 # Create your views here.
 def index(request):
