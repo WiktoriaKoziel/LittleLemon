7672ba21771305976835f33b4a85e09b6f7cec7e
WiktoriaKoziel <wiktoria.koziel@gmail.com>
2023-08-21 10:24:18 +0200

    api

--- workspace/littlelemon/restaurant/views.py
+++ workspace/littlelemon/restaurant/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .serializers import MenuSerializer
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+from django.core import serializers
+from .models import Booking, Menu
+import json
+from rest_framework.viewsets import ModelViewSet
+from .serializers import BookingSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
+    
+class MenuItemsView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
