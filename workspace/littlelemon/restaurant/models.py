1801cff1cf9a4986e03f6dc40598b6042ec1e4d9
WiktoriaKoziel <wiktoria.koziel@gmail.com>
2023-08-21 09:19:49 +0200

    mysql

--- workspace/littlelemon/restaurant/models.py
+++ workspace/littlelemon/restaurant/models.py
@@ -1,3 +1,16 @@
+import calendar
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(default=0)
+
+class Booking(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    no_of_guest = models.IntegerField(default=0)
+    bookingDate = models.DateField(default='2000-01-01')
+    
